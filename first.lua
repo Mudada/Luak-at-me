506a3b9537dad4d92fc3dfffaf15949510291b86
Mudada <nicola_m@etna-alternance.net>
2016-09-08 23:20:51 +0200

    logical op

--- first.lua
+++ first.lua
@@ -1,10 +1,31 @@
---references
+--[[ references ]]
 
 t1 = {1, 2, 3}
 t2 = {1, 2, 3}
-
 print(t1)
 print(t2)
 print(t1 == t2) -- false
 t2 = t1
 print(t1 == t2) -- true
+
+
+--[[ logical op ]]
+
+if nil then -- false
+print "hey" 
+end
+
+foo = false
+bar = "you"
+
+print(foo or bar) -- you
+
+print(not foo) -- true
+print(not "is it") -- false
+
+print(foo and bar) -- false
+print("you" and "are" and "looking") -- "looking"
+
+print(true or print("for")) -- true
+print(false or print("for")) -- for && nil : print->
+print(print("for") or true) -- "for" && true
