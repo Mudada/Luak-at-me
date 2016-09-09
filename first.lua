f0db5950952d9948e7e1d5a45053505333452a8e
Mudada <nicola_m@etna-alternance.net>
2016-09-09 20:01:02 +0200

    the foreach in lua look like really stronk

--- first.lua
+++ first.lua
@@ -9,7 +9,7 @@ t2 = t1
 print(t1 == t2) -- true
 
 
---[[ logical op ]]
+--[[ logical op ]] -- OUFF
 
 if nil then -- false
 print "hey" 
@@ -29,3 +29,26 @@ print("you" and "are" and "looking") -- "looking"
 print(true or print("for")) -- true
 print(false or print("for")) -- for && nil : print->
 print(print("for") or true) -- "for" && true
+
+function Hello(str)
+    str = str or "World"
+    print("Hello, ", str)
+end
+
+Hello(); -- "Hello, World"
+Hello("Bibou") -- "Hello, Bibou"
+
+
+-- [[ Definitly not object ]]
+
+t = {}
+t.foo = function(x) print(x) end
+t.foo("plop")
+t:foo() -- table : a;skd;asd => foo(t)
+
+-- [[ Lua is better than foreach ]]
+
+tabl = {a = 1, b = 2, c = 3, d = 4, e = 5, f = 6}
+for key, value in next, tabl do -- function -> i++
+    print ("Key: "..key, "Value: "..value)
+end
